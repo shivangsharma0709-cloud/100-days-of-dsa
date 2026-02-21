4460e01a225bef7af23fb28f6fc6b5693931306d
shivangsharma0709-cloud <shivangsharma0709@gmail.com>
2026-02-21 22:43:20 +0530

    Add files via upload

---
+++ dsad19.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n]; 
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    qsort(arr, n, sizeof(int), compare); 
+    int left = 0, right = n - 1;
+    int minSum = __INT_MAX__;
+    int minLeft = left, minRight = right;
+    while (left < right) {
+        int sum = arr[left] + arr[right]; 
+        if (abs(sum) < abs(minSum)) {
+            minSum = sum;
+            minLeft = left;
+            minRight = right;
+        }
+        if (sum < 0)
+            left++;
+        else
+            right--;
+    } 
+    printf("%d %d\n", arr[minLeft], arr[minRight]);
+    return 0;
+}
