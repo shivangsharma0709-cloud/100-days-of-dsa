ed687a6b850b473ddb403f072fdecea7cce2e23f
shivangsharma0709-cloud <shivangsharma0709@gmail.com>
2026-02-24 15:30:15 +0530

    Add files via upload

---
+++ dsad22.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+int countNodes(struct Node* head) {
+    int count = 0;
+    struct Node* temp = head;
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    struct Node* head = NULL;
+    struct Node* tail = NULL;
+    for (int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+        struct Node* newNode = createNode(value);
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    printList(head);
+    printf("\n");
+    printf("Number of nodes = %d\n", countNodes(head));
+    return 0;
+}
