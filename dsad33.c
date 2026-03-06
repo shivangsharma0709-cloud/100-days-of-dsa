fd6420bf4283fb7a6343d50c2eaf0dd4039d8f37
shivangsharma0709-cloud <shivangsharma0709@gmail.com>
2026-03-06 21:39:36 +0530

    Add files via upload

---
+++ dsad33.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char c) {
+    stack[++top] = c;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+int precedence(char op) {
+    if (op == '^') return 3;
+    if (op == '*' || op == '/') return 2;
+    if (op == '+' || op == '-') return 1;
+    return 0;
+}
+
+int isOperator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+    int i, j = 0;
+    scanf("%s", infix);
+    for (i = 0; i < strlen(infix); i++) {
+        char c = infix[i];
+        if (isalnum(c)) {
+            postfix[j++] = c;
+        } else if (c == '(') {
+            push(c);
+        } else if (c == ')') {
+            while (top != -1 && stack[top] != '(')
+                postfix[j++] = pop();
+            pop();
+        } else if (isOperator(c)) {
+            while (top != -1 && precedence(stack[top]) >= precedence(c))
+                postfix[j++] = pop();
+            push(c);
+        }
+    }
+    while (top != -1)
+        postfix[j++] = pop();
+    postfix[j] = '\0';
+    printf("%s\n", postfix);
+    return 0;
+}
